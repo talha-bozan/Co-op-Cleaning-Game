e5b2f9135fa8b8ef2f99818da20d164d46db5f3e
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-07-14 12:25:12 +0300

    char trash and bin trash slider

--- Assets/Scripts/Character/VacuumCleaner.cs
+++ Assets/Scripts/Character/VacuumCleaner.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VacuumCleaner : MonoBehaviour
 {
     private int _totalCapacity = 20;
-    private int _collectedTrashAmount;
+    public int _collectedTrashAmount;
+    [SerializeField] private Slider _trashBinSlider;
 
+    private void Start()
+    {
+        _trashBinSlider.minValue = 0;
+        _trashBinSlider.maxValue = _totalCapacity - 1;
+    }
 
     public void DecreaseCollectedCount()
-    {
+    {  
         _collectedTrashAmount -= 1;
+        _trashBinSlider.value = _collectedTrashAmount;
     }
 
     public bool CheckIfThereIsSpace()
     {
         if (_collectedTrashAmount < _totalCapacity)
-        {
+        {  
             _collectedTrashAmount += 1;
+            _trashBinSlider.value = _collectedTrashAmount;
             Debug.Log($"Total trash: {_collectedTrashAmount}");
             return true;
         }
