d9383fdbaf88b25053dfd0d74128ef6d8a7d955e
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-07-19 12:54:57 +0300

    test for interaction between objects

---
+++ Assets/Scripts/SteamHostClientScripts/testtttt.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+
+public class testtttt : NetworkBehaviour
+{
+
+    [SyncVar] public int health = 4;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    [ServerCallback]
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other == this.gameObject) { return; }
+
+            --other.GetComponent<testtttt>().health;
+            
+        }
+    }
+}
