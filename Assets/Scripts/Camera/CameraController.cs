342185f6d11a9dae8fc7c877d6825f690d8f9333
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-07-20 20:01:33 +0300

    individual camera component

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Mirror;
+public class CameraController : NetworkBehaviour
+{
+
+    public GameObject cameraHolder;
+    public Vector3 offset;
+
+    public override void OnStartAuthority()
+    {
+        cameraHolder.SetActive(true);
+    }
+    public void Update()
+    {
+
+
+        if (SceneManager.GetActiveScene().name == "4PlayerGameScene")
+        {
+            cameraHolder.transform.position = transform.position + offset;
+        }
+    }
+        
+}
