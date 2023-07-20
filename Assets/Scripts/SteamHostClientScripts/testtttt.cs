6db19c51c593ae7b2f3ceff3154334ae73aee68f
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-07-20 11:26:38 +0300

    test bilmem kac

--- Assets/Scripts/SteamHostClientScripts/testtttt.cs
+++ Assets/Scripts/SteamHostClientScripts/testtttt.cs
@@ -3,28 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-
 public class testtttt : NetworkBehaviour
 {
-
     [SyncVar] public int health = 4;
     [SerializeField] private LayerMask pLayerMask;
 
     // Update is called once per frame
     void Update()
     {
-        if(isLocalPlayer)
+        if (isLocalPlayer)
         {
-            detector();
+            DetectPlayers();
         }
     }
 
-
-
-    [Command]
-    void detector()
+    void DetectPlayers()
     {
-
         Collider[] playersInRange = Physics.OverlapSphere(this.transform.position, 2f, pLayerMask);
 
         foreach (Collider player in playersInRange)
@@ -32,11 +26,21 @@ public class testtttt : NetworkBehaviour
             if (player.gameObject == this.gameObject) // Skip over the "owner" player
                 continue;
 
-            if (Input.GetKey(KeyCode.LeftControl)){ Debug.Log("bldu"); }
-                
-            
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                CmdHandleDetection(player.gameObject);
+            }
+        }
+    }
 
+    [Command]
+    void CmdHandleDetection(GameObject detectedPlayer)
+    {
 
+        testtttt playerScript = detectedPlayer.GetComponent<testtttt>();
+        if (playerScript != null)
+        {
+            playerScript.health--;
         }
     }
 }
