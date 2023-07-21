df2d7805591aaecdd7c920e14e0cd8f28067dfc1
Mehmet Talha Bozan <bozanm@mef.edu.tr>
2023-07-21 13:03:21 +0300

    camera's position bug fix

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -6,19 +6,35 @@ using Mirror;
 
 public class CameraController : NetworkBehaviour
 {
+    public Transform playerBody; // Reference to the player's body (rotation controller)
+    public float smoothSpeed = 0.125f;
+    public float rotationSpeed = 5f;
     public GameObject cameraHolder;
     public Vector3 offset;
+
+    private Vector3 desiredPosition;
+    private Quaternion desiredRotation;
 
     public override void OnStartAuthority()
     {
         cameraHolder.SetActive(true);
     }
-
-    public void Update()
+    private void LateUpdate()
     {
-        if (SceneManager.GetActiveScene().name == "4PlayerGameScene")
-        {
-            cameraHolder.transform.position = transform.position + offset;
-        }
+        if (!isOwned || playerBody == null)
+            return;
+
+        // Calculate the desired position and rotation based on the player's body
+        desiredPosition = playerBody.position + playerBody.TransformDirection(playerBody.position+ offset);
+        desiredRotation = Quaternion.Euler(0, playerBody.eulerAngles.y, 0);
+
+        // Smoothly move the camera to the desired position and rotation
+        SmoothFollow();
+    }
+      private void SmoothFollow()
+    {
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed) ;
+        transform.rotation = Quaternion.Slerp(transform.rotation, desiredRotation, rotationSpeed * Time.deltaTime);
+    
     }
 }
